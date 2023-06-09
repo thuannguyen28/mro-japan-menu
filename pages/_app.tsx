a5d5fce89c49fd3eddc5c06cb00702b78d7ea146
Phuc Nguyen <nhphuc@starboardasia.com>
2023-06-09 15:16:35 +0700

    update structure component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
-import MainNav from './components/layout/MainNav';
+import MainNav from '../components/layout/MainNav';
 import '../styles/globals.css';
 
 // Client-side cache, shared for the whole session of the user in the browser.
