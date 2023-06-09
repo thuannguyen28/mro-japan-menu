7323a0c24fb84a30e2547bd6ff850e216a54fc71
Phuc Nguyen <nhphuc@starboardasia.com>
2023-06-09 16:21:45 +0700

    remove cache provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider, EmotionCache } from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
 import MainNav from '../components/layout/MainNav';
@@ -12,32 +11,22 @@ import '../styles/globals.css';
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
-export interface MyAppProps extends AppProps {
-  emotionCache?: EmotionCache;
-}
+export interface MyAppProps extends AppProps {}
 
 export default function MyApp(props: MyAppProps) {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles?.parentElement?.removeChild(jssStyles);
-    }
-  }, []);
+  const { Component, pageProps } = props;
 
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <title>MRO Japan</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <MainNav mainPage={<Component {...pageProps} />}/>
-      </ThemeProvider>
-    </CacheProvider>
+      <div>
+        <Head>
+          <title>MRO Japan</title>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <MainNav mainPage={<Component {...pageProps} />}/>
+        </ThemeProvider>
+      </div>
   );
 }
