8725fed293fc53da31cefcde49c2ff01523e2a43
Phuc Nguyen <nhphuc@starboardasia.com>
2023-06-27 13:54:03 +0700

    update

--- pages/part_kit/part_kit_insert.tsx
+++ pages/part_kit/part_kit_insert.tsx
@@ -1287,7 +1287,7 @@ export default function RequestPartMaterials() {
                           }}
                           sx={{ ml:4}}
                         >
-                          <MenuItem onClick={handleClickOpenSearch}><Typography sx={cssMenu} >View</Typography></MenuItem>
+                          <MenuItem onClick={handleClickOpenView}><Typography sx={cssMenu} >View</Typography></MenuItem>
                         </Menu>
                       </Grid>
                       <Grid item xs={1.5}>
@@ -1779,12 +1779,198 @@ export default function RequestPartMaterials() {
             </Box>
           </DialogContentText>
         </DialogContent>
-
       </Dialog>
     {/*----------End Dialog memo----------*/}
 
+      {/*Dialog memo 2*/}
+     <Dialog open={openView} onClose={handleCloseView} maxWidth="xl">
+        <DialogTitle sx={{ py: 4, px: 0 }}>
+          <BootstrapDialogTitle sx={{ py: 1, px: 2 }} id="customized-dialog-title" onClose={handleCloseView}> 
+            <Grid container spacing={2}>
+              <Grid item xs={10}>
+                    <Typography sx={{ width: '100%', color: '#325381', textAlign: 'left', fontSize: '12px', fontWeight: 700, mb: '5px', mt: 0 }}>資村出庫要求伝票  差戻し確認</Typography>
+              </Grid>
+              <Grid item xs={2}>
+                    <Typography sx={{ width: '100%', color: '#325381', textAlign: 'right', fontSize: '12px', fontWeight: 700, mb: '5px', mt: 0 }}>資村出庫要求伝票  差戻し確認</Typography>
+              </Grid>
+              <Grid item xs={7} md={9}>
+                <CardMedia
+                  sx={{ width: { xs: 150, md: 222} , height: { xs: 40, md: 58}}}
+                  image="../part-order/anal_1.png"
+                  title="ANA"
+                />
+              </Grid>
+              <Grid item xs={5} md={3} sx={{ position: 'relative' }}>  
+                <Link href="https://www.mrojpn.co.jp" underline="none" target="_blank">
+                  <Typography sx={cssMro2}>
+                  https://www.mrojpn.co.jp
+                  </Typography>
+                </Link>
+              </Grid>
+            </Grid>
+            <Divider sx={{ my: 1}} />
+          </BootstrapDialogTitle>
+        </DialogTitle>
 
-    
+        <DialogContent>
+          <DialogContentText>
+            <Box
+              sx={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                color: '#325381',
+                '& > :not(style)': {
+                  width: 1200,
+                  height: 700,
+                  ml:2,
+                  mr:1,
+                },
+              }}
+            >
+              <Paper elevation={0}>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <Typography sx={cssOder}>21 - 15292</Typography>
+                  </Grid>
+                </Grid>
+
+                <Grid container spacing={2}>
+                  <Grid item xs={12} md={3}>
+                      <Typography sx={{ fontSize: '16px' }}>Request For Part And Materials</Typography>
+                      <Typography sx={{ fontSize: '14px', mt: 1, mb: 1 }}>資村出庫要求伝票</Typography>
+                      <Box sx={cssBox}>
+                        <Grid container sx={{ height: '58px' }}>
+                          <Grid item xs>
+                            <Typography sx={{ fontSize: '12px', ml: 1, mt: '10px' }}>Issued Parts</Typography>
+                            <Typography sx={{ fontSize: '12px', ml: 1 }}>出庫資村</Typography>
+                          </Grid>
+                          <Divider orientation="vertical" flexItem />
+                          <Grid item xs>
+                            <Grid container spacing={0} sx={{ mt: '10px' }}>
+                              <Grid item xs={4}>
+                                <Checkbox inputProps={{ 'aria-label': 'controlled' }} />
+                              </Grid>
+                              <Grid item xs={8}>
+                                <Typography sx={font12}>MJP</Typography>
+                                <Typography sx={font12}>自社資村</Typography>
+                              </Grid>
+                            </Grid>
+                          </Grid>
+                          <Divider orientation="vertical" flexItem />
+                          <Grid item xs>
+                            <Grid container spacing={0} sx={{ mt: '10px' }}>
+                              <Grid item xs={4}>
+                                <Checkbox inputProps={{ 'aria-label': 'controlled' }} />
+                              </Grid>
+                              <Grid item xs={8}>
+                                <Typography sx={font12}>Customer</Typography>
+                                <Typography sx={font12}>支給資村</Typography>
+                              </Grid>
+                            </Grid>
+                          </Grid>
+                        </Grid>
+                      </Box>
+                  </Grid>
+                  <Grid item xs={12} md={3}>
+                    <Box sx={cssBox}>
+                      <Typography sx={font12Center}>Requested by / 資村要求者</Typography>
+                      <Divider sx={{ my: '4px'}} />
+                      <Grid container>
+                        <Grid item xs>
+                          <Typography sx={font12Center}>Zone</Typography>
+                        </Grid>
+                        <Divider orientation="vertical" flexItem />
+                        <Grid item xs>
+                          <Typography sx={font12Center}>Name</Typography>
+                        </Grid>
+                      </Grid>
+                      <Divider sx={{ my: '4px'}} />
+                      <Grid container>
+                        <Grid item xs>
+                          <Typography sx={font12Center}></Typography>
+                        </Grid>
+                        <Divider orientation="vertical" flexItem />
+                        <Grid item xs>
+                          <Typography sx={{ fontSize: '12px', textAlign: 'center', height: '65px' }}></Typography>
+                        </Grid>
+                      </Grid>
+                    </Box>
+                  </Grid>
+                  <Grid item xs={12} md={3}>
+                    <Box sx={cssBox}>
+                      <Typography sx={font12Center}>Received By / 資村受領者</Typography>
+                      <Divider sx={{ my: '4px'}} />
+                      <Grid container>
+                        <Grid item xs>
+                          <Typography sx={font12Center}>Zone</Typography>
+                        </Grid>
+                        <Divider orientation="vertical" flexItem />
+                        <Grid item xs>
+                          <Typography sx={font12Center}>Name</Typography>
+                        </Grid>
+                      </Grid>
+                      <Divider sx={{ my: '4px'}} />
+                      <Grid container>
+                        <Grid item xs>
+                          <Typography sx={{ fontSize: '12px', textAlign: 'center', height: '65px' }}></Typography>
+                        </Grid>
+                        <Divider orientation="vertical" flexItem>
+                        </Divider>
+                        <Grid item xs>
+                          <Typography sx={{ fontSize: '12px', textAlign: 'center', height: '65px' }}></Typography>
+                        </Grid>
+                      </Grid>
+                    </Box>          
+                    
+                  </Grid>
+                  <Grid item xs={12} md={3}>
+                    <Grid container spacing={2}>
+                      <Grid item xs={6}>
+                        <Box sx={cssBox}>
+                          <Typography sx={font12Center}>Supply Section</Typography>
+                          <Typography sx={font12Center}>出庫担当者</Typography>
+                          <Divider sx={{ my: '13px'}} />
+                          <Grid container>
+                            <Grid item xs>
+                              <Typography sx={{ fontSize: '12px', textAlign: 'center', height: '56px' }}></Typography>
+                            </Grid>
+                          </Grid>
+                        </Box>
+                      </Grid>
+                      <Grid item xs={6}>
+                        <Box sx={cssBox5}>
+                          <Grid container spacing={1}>
+                            <Grid item xs={12}>
+                              <CardMedia
+                                sx={{ width: 97 , height: 97, margin: 'auto' }}
+                                image="../qr_code/QR 1.png"
+                                title="ANA"
+                              />
+                            </Grid>
+                          </Grid>
+                        </Box>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
+               
+                <ListPart />
+
+                <Grid container spacing={0} sx={{ mt : 2 }}>
+                  <Grid item xs={12} md={9}>
+                   <Typography sx={{ fontSize : '12px' }}>Note : 　① 　A/C-No. = Aircraft Registration No./ 機体登録　No.</Typography>
+                   <Typography sx={{ fontSize : '12px', ml: '50px' }}>②　RSN. = Reason Code / 部品消費理由コード (TC = MR TASK CARD / EO = EO 作業 / TR = TRP / RC = Discrepancy Repair 不具合修復 / MQ = Maintenance Quality 作業品質関連 / OT = Other その他)</Typography>          
+                  </Grid>
+                  <Grid item xs={12} md={3}>
+                    <Typography sx={{ fontSize : '12px', textAlign: 'right', mt: 2 }}>MRO Japan Form - SD - 004C</Typography>
+                  </Grid>
+                </Grid>
+              </Paper>
+            </Box>
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
+    {/*----------End Dialog memo2----------*/}
 
   </div>
   );
