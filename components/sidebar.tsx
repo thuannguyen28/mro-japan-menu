4b7ae83a6e2845e015164a6f37354e4f95f77d29
HoNgocNguyenSBA <hnnguyen@starboard-asia.com>
2023-07-14 13:30:46 +0700

    update 3-1LINE

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -312,12 +312,14 @@ export default function NestedList(props) {
 		    </ListItemButton>
 		    <Collapse in={open[7]} timeout="auto" unmountOnExit>
 		      <List component="div" disablePadding>
-		        <ListItemButton sx={{ pl: 4 }}>
-		          <ListItemIcon>
-		            <StarBorder sx={color} />
-		          </ListItemIcon>
-		          <ListItemText primaryTypographyProps={{ style: cssMenuLink }} primary="Tools Order" />
-		        </ListItemButton>
+                <Link href="/tools_order" sx={cssLink}>
+                    <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemIcon>
+                        <StarBorder sx={color} />
+                    </ListItemIcon>
+                    <ListItemText primaryTypographyProps={{ style: cssMenuLink }} primary="Tools Order" />
+                    </ListItemButton>
+                </Link>
 		      </List>
 		    </Collapse>
 
